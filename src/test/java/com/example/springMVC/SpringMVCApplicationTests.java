1deedfff194efec9c51c02b4190724e6f072994e
good0619@naver.com <32181771@dankook.ac.kr>
2023-03-10 16:55:14 +0900

    refactor : modify file & package name

---
+++ src/test/java/com/example/springMVC/SpringMVCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMVCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
