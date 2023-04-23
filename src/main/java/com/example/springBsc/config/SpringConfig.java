f7a3464c74865e10f3edf5d32b95423bbc6fd57f
good0619@naver.com <32181771@dankook.ac.kr>
2023-04-23 12:30:09 +0900

    feat : implement connection to h2-database

--- src/main/java/com/example/springBsc/config/SpringConfig.java
+++ src/main/java/com/example/springBsc/config/SpringConfig.java
@@ -1,13 +1,21 @@
-package com.example.springBsc;
+package com.example.springBsc.config;
 
+import com.example.springBsc.repository.JdbcMemberRepository;
 import com.example.springBsc.repository.MemberRepository;
-import com.example.springBsc.repository.MemoryMemberRepository;
 import com.example.springBsc.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource datasource;
+
+    public SpringConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(datasource);
     }
 }
